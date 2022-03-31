0f4f71790a3ddd42fea0910b3c10b19462f30899
Paul Gottinger <pgottinger@outlook.com>
2022-03-30 20:14:23 -0500

    login/delete

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(require("./controllers"));
 
 // turn on routes
 app.use(routes);
